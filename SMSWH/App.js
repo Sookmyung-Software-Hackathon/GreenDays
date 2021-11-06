f1fdf804e1e3f4126e4d222af1b83497d0ecb956
minjung-sys <cho9906@naver.com>
2021-11-06 20:38:42 +0900

    회원가입 로그인

--- SMSWH/App.js
+++ SMSWH/App.js
@@ -1,21 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
- import Fontawesome from 'react-native-vector-icons/FontAwesome';
+import Fontawesome from 'react-native-vector-icons/FontAwesome';
  import React from 'react';
- import {SafeAreaView, Text} from 'react-native';
+ //import {SafeAreaView, Text} from 'react-native';
  import {NavigationContainer} from '@react-navigation/native'
- import { createStackNavigator } from '@react-navigation/stack';
- import Home from './src/components/Home';
- import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
- const Stack = createStackNavigator();
- 
- const Tabs = createBottomTabNavigator();
+// import { createStackNavigator } from '@react-navigation/stack';
+import Home  from './src/components/Home';
+import signin from './src/components/signin';
+import signup from './src/components/signup';
 
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createStackNavigator} from '@react-navigation/stack'
+ //const Stack = createStackNavigator();
+   
+const Tabs = createBottomTabNavigator();
+
+const homeStackNavigation = createStackNavigator();
+const signStackNavigation = createStackNavigator();
+signStackscreen = () =>{
+  return (
+    <signStackNavigation.Navigator screenOptions = {({route})=>({headerShown:false})}>
+      <signStackNavigation.Screen name = "signin" component={signin}/>
+      <signStackNavigation.Screen name = "signup" component={signup}/>
+     
+    </signStackNavigation.Navigator>
+  )
+}
+HomeStackScreen = ()=>{
+  return(
+    <homeStackNavigation.Navigator screenOptions = {({route})=>({headerShown:false})}>
+      <homeStackNavigation.Screen name="home" component={Home}/>
+    </homeStackNavigation.Navigator>
+  )
+}
 class App extends React.Component{
   render(){
     return(
@@ -26,7 +41,7 @@ class App extends React.Component{
               let iconName;
               if(route.name =='Ranking'){
                 iconName = 'home'
-              }else if (route.name == 'Home'){
+              }else if (route.name == 'HomeStack'){
                iconName= 'comment'
               }
               else if(route.name == 'mypage'){
@@ -44,9 +59,11 @@ class App extends React.Component{
         
         
         >
-        <Tabs.Screen name = "Home" component={Home}/>
+          <Tabs.Screen name = 'sign' component ={signStackscreen}/>
+          <Tabs.Screen name = "HomeStack" component={HomeStackScreen}/>
+        
+        
         </Tabs.Navigator>
-      
       </NavigationContainer>
     )
   }
---
+++ SMSWH/src/components/signup.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { useState } from 'react';
+import {View,Text,StyleSheet,Button,SafeAreaView,TouchableOpacity, Alert} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { TextInput, TouchableHighlight } from 'react-native-gesture-handler';
+
+function signup({navigation}){
+  
+    const [userEmail,setuserEmail] = useState('');
+    const [userPassword,setuserPassword] = useState('');
+    const [confirmPassword,setconfirmPassword]= useState('');
+    return(
+    <SafeAreaView style = {signupstyle.fullscreen}>
+        <View style = {signupstyle.all}>
+          <View>
+              <Text style = {signupstyle.title}>회원가입</Text>
+          </View>
+          <View>
+              <TextInput 
+                style = {signupstyle.textinput}
+                placeholder="이메일을 입력해주세요"
+                onChange={(e)=>{
+                    const {eventCount,target,text}=e.nativeEvent;
+                    setuserEmail(text);
+                }}
+
+              />
+              <TextInput  
+                style = {signupstyle.textinput}
+                placeholder="비밀번호를 입력해주세요"
+                onChange={(e)=>{
+                    const {eventCount,target,text}=e.nativeEvent;
+                    setuserPassword(text);
+                }}
+
+              />
+              <TextInput  
+                style = {signupstyle.textinput}
+                placeholder="비밀번호를 재입력해주세요"
+                onChange={(e)=>{
+                    const {eventCount,target,text}=e.nativeEvent;
+                    setconfirmPassword(text);
+                }}
+
+              />
+          </View>
+          <TouchableOpacity style = {signupstyle.button}>
+              <Text style = {signupstyle.buttontext}>
+                  등록
+              </Text>
+          </TouchableOpacity>
+        </View>
+    </SafeAreaView>
+    )
+
+}
+signupstyle = StyleSheet.create({
+    fullscreen:{
+        height:800,
+        backgroundColor:"#BEDBBB",
+    },
+    all :{
+        backgroundColor:"#BEDBBB",
+        alignItems:'center',
+        flexDirection:'column',
+        top: 250,
+       
+
+    },
+    title:{
+        
+        fontSize :40,
+        margin:10,
+        marginBottom:20
+
+        
+       
+
+    },
+    subtitle:{
+        fontsize:40,
+        top: 290,
+        left : -30,
+
+    },
+    textinput :{
+        backgroundColor:'white',
+        width:280,
+        height:40,
+        borderRadius:15,
+        margin:10
+
+
+    },
+    button:{
+        width:200,
+        height:35,
+        backgroundColor:"#FEF5ED",
+        alignItems:'center',
+        margin:10,
+        borderRadius:8
+    },
+    buttontext:{
+        top:10,
+        fontSize:20
+    }
+})
+export default signup;
